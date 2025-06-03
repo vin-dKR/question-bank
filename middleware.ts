1cd46f8440673a716edf558d6f181b0475b09622
vin-dKR <vinodkumarmurmu62@gmail.com>
2025-06-03 17:38:06 +0530

    fixed: middleware in production

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher(['/api/questions(.*)']);
